b3d7daaa2bee12a11c49a03cf35f939642d3f9f4
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-08-29 10:04:42 -0400

    write validateEmail function and connect to contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
     return (
+        // Contact form with name, email, message
+        // all fields required, notification if empty
+        // email validation
         <div>
             <h1>Hello, Contact!</h1>
         </div>
