1df12d2ce9b08a33bb2a80365b661735e2554447
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-08-29 14:24:44 -0400

    drying up proficiencies

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Resume() {
+
+    const [backendProficiencies] = useState([
+        'APIs',
+        'Node',
+        'Express'
+    ])
+
     return (
-        <h1>Hello, resume!</h1>
+        <section>
+
+            <h1>RESUME</h1>
+
+            <a href='https://docs.google.com/document/d/1Os-AhB34JOiGA0scCg0NLdAz1jedzayccGXgS6Q7OGI/edit?usp=sharing' target='_blank'>
+                <button>Download technical resume</button>
+            </a>
+
+            <h2>Front-end Proficiencies</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>jQuery</li>
+                    <li>Responsive Design</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                </ul>
+
+            <h2>Back-end Proficiencies</h2>
+                <ul>
+                    {/* as we go through the backendProficiencies array, we are naming each individual item 'proficiency' */}
+                    {backendProficiencies.map((proficiency, i) => (
+                        <li>{backendProficiencies[i]}</li>
+                    ))}
+                </ul>
+
+        </section>
     )
 };
 
