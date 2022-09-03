1897a9b79aa2c54464a14a8817889bb9c3e9397c
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-02 22:56:56 -0400

    hero and about responsive styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,13 +14,13 @@ function About() {
                 {/* <img src={headshot} alt='headshot' style={{ width: '50%' }} /> */}
             </section>
 
-            <section>
-                <p>
-                    I'm a full-stack web developer and an upcoming graduate from the University of North Carolina at Chapel Hill.
-                    Newly developed skills include JavaScript, managing databases, responsive web design, and following the Model-View-Controller paradigm. Strengths in creativity, teamwork, and building projects from ideation to execution.
-                    I'm currently focusing on completing my certificate and will be open to full-time, contract, and freelancing opportunities.
-                </p>
+            <div className='d-flex flex-row justify-content-center'>
+            <section className='about  col-12 col-md-8'>
+                <p className='font-size-1halfrem'>I'm a full-stack web developer and an upcoming graduate from the University of North Carolina at Chapel Hill.</p>
+                <p className='font-size-1halfrem'>Newly developed skills include JavaScript, managing databases, responsive web design, and following the Model-View-Controller paradigm. Strengths in creativity, teamwork, and building projects from ideation to execution.</p>
+                <p className='font-size-1halfrem'>I'm currently focusing on completing my certificate and will be open to full-time, contract, and freelancing opportunities.</p>
             </section>
+            </div>
 
         </div>
     )
