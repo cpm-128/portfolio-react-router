e2ae298a5d87d067c7c7549043ddd24c1e962f0a
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-22 12:48:43 -0400

    tweak to header/nav alignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return (
-        <div className='padding-top-0'>
+        <div className='padding-top-0 padding-left-header'>
             <h1 className='text-color-light bg-color-primary padding-standard font-size-p light-font-weight'>
                 Colleen Maher | Full-Stack Web Developer
             </h1>
