b8f56dc653953f549e0197863efbb9af978257d3
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-22 12:09:21 -0400

    add race cards added

--- src/components/pages/Cycling.js
+++ src/components/pages/Cycling.js
@@ -14,7 +14,68 @@ function Cycling() {
             feetClimbing: '3,400',
             hours: '2',
             minutes: '39',
+            when: 'Sep 2022',
             results: 'https://results.raceroster.com/v2/en-US/results/d4gwk7qpfahdf396/results?subEvent=122275&page=1&genderSexId=36eda1e6-c9ff-4038-802d-2b4b178c7753',
+        },
+        {
+            title: 'Dirt Diggler',
+            type: 'Gravel Race',
+            image: require('../../assets/images/cycling/dirt-diggler_2022.JPG'),
+            result: '1st Place Womens Overall, 5th Person Overall',
+            miles: '27',
+            feetClimbing: '2,500',
+            hours: '2',
+            minutes: '00',
+            when: 'Sep 2022',
+            results: 'https://www.blueridgeadventures.net/2022-dirt-diggler-gravel-grinder-results/',
+        },
+        {
+            title: 'SEG Falling Creek Pinnacle Punisher',
+            type: 'Gravel Race',
+            image: require('../../assets/images/cycling/pinnacle-punisher_2022.JPG'),
+            result: '1st Place Womens 19-39',
+            miles: '67',
+            feetClimbing: '5,000',
+            hours: '4',
+            minutes: '26',
+            when: 'Sep 2022',
+            results: 'https://southeastgravel.com/pages/2022-falling-creek-results',
+        },
+        {
+            title: 'Sasquatch Scrambler',
+            type: 'Mountain Bike Race',
+            image: require('../../assets/images/cycling/sasquatch_2022.JPG'),
+            result: '2nd Place Womens Open',
+            miles: '15',
+            feetClimbing: '2,000',
+            hours: '1',
+            minutes: '49',
+            when: 'Jun 2022',
+            results: 'https://www.blueridgeadventures.net/2022-sasquatch-scrambler-results/',
+        },
+        {
+            title: 'Swamp Fox Gravel Fondo',
+            type: 'Gravel Race',
+            image: require('../../assets/images/cycling/swamp-fox_2022.JPG'),
+            result: '3rd Place Womens Open',
+            miles: '100',
+            feetClimbing: '80',
+            hours: '6',
+            minutes: '20',
+            when: 'Mar 2022',
+            results: 'https://www.webscorer.com/racedetails?raceid=271115&did=317143&cid=1570449',
+        },
+        {
+            title: 'Dirt Diggler',
+            type: 'Gravel Race',
+            image: require('../../assets/images/cycling/dirt-diggler_2021.JPG'),
+            result: '3rd Place Womens open',
+            miles: '27',
+            feetClimbing: '2,500',
+            hours: '2',
+            minutes: '19',
+            when: 'Sep 2021',
+            results: 'https://www.blueridgeadventures.net/2021-dirt-diggler-gravel-grinder-results/',
         },
     ])
 
@@ -44,6 +105,7 @@ function Cycling() {
                                 <li>{event.miles} miles</li>
                                 <li>{event.feetClimbing} feet climbing</li>
                                 <li>{event.hours} hours, {event.minutes} minutes</li>
+                                <li>{event.when}</li>
                             </ul>
                             <a href={event.results} className='card-link' target='_blank' rel='noopener noreferrer'>Race Results</a>
                         </div>
