c0ecf256285b24eeccb80201535537fb8ac8aa75
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-12-20 13:06:36 -0500

    updated about bio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,9 +16,9 @@ function About() {
 
             <div className='d-flex flex-row justify-content-center'>
             <section className='about  col-12 col-md-8'>
-                <p className='font-size-1halfrem'>I'm a full-stack web developer and an upcoming graduate from the University of North Carolina at Chapel Hill.</p>
-                <p className='font-size-1halfrem'>Newly developed skills include JavaScript, managing databases, responsive web design, and following the Model-View-Controller paradigm. Strengths in creativity, teamwork, and building projects from ideation to execution.</p>
-                <p className='font-size-1halfrem'>I'm currently focusing on completing my certificate and will be open to full-time, contract, and freelancing opportunities.</p>
+                <p className='font-size-1halfrem'>I'm a full-stack web developer and recently graduated the University of North Carolina at Chapel Hill's Web Dev Bootcamp with a 4.0.</p>
+                <p className='font-size-1halfrem'>Newly developed skills include JavaScript, managing databases, responsive web design, and following best practices for code organization and structure. I  am a thorough, detail-oriented, and conscientious coder whose strengths include creativity, teamwork, and building projects from ideation to execution.</p>
+                <p className='font-size-1halfrem'>I am open to full-time, contract, and freelancing opportunities.</p>
             </section>
             </div>
 
