05751b3d9562acbfa6c12c1375cef080929b69bf
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-12-20 23:37:31 -0500

    basename to router

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -14,6 +14,7 @@ import Cycling from './pages/Cycling';
 function PortfolioContainer() {
 
     return (
+        // the basename will keep the entire homepage url, even with slugs
         <Router basename={process.env.PUBLIC_URL}>
             <div className='portfolio-container d-flex flex-column min-vh-100'>
                 <NavTabs />
